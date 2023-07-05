fae5e32dd96496dddb6656e7a4949990c34f8681
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-07-05 15:55:13 +0300

    Initial Commit

---
+++ CardCreationSwiftUI/CardCreationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardCreationSwiftUIApp.swift
+//  CardCreationSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 5.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct CardCreationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardCreationSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CardCreationSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 5.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
