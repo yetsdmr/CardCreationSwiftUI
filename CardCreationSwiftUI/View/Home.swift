11b9024827cfc75b2498a5a37dee92966ff63f79
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-07-06 14:16:31 +0300

    Building card layout.

--- CardCreationSwiftUI/View/Home.swift
+++ CardCreationSwiftUI/View/Home.swift
@@ -8,6 +8,12 @@
 import SwiftUI
 
 struct Home: View {
+    // View Properties
+    @FocusState private var activeTF: ActiveKeyboardField!
+    @State private var cardNumber: String = ""
+    @State private var cardHolderName: String = ""
+    @State private var cvvCode: String = ""
+    @State private var expireDate: String = ""
     var body: some View {
         VStack {
             // Header View
@@ -35,8 +41,67 @@ struct Home: View {
             }
             
             // Card View
+            CardView()
+            
+            Spacer(minLength: 0)
         }
         .padding()
+    }
+    
+    @ViewBuilder
+    func CardView() -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(.linearGradient(colors: [
+                    Color("CardGradient2"),
+                    Color("CardGradient1")
+                ], startPoint: .topLeading, endPoint: .bottomTrailing))
+        
+            // Card Details
+            VStack(spacing: 10) {
+                HStack {
+                    TextField("XXXX XXXX XXXX XXXX", text: $cardNumber)
+                        .font(.title3)
+                        .keyboardType(.numberPad)
+                        .focused($activeTF, equals: .cardNumber)
+                    
+                    Spacer(minLength: 0)
+                    
+                    Image("Visa")
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 20)
+                }
+                
+                HStack(spacing: 12) {
+                    TextField("MM/YY", text: $expireDate)
+                        .keyboardType(.numberPad)
+                        .focused($activeTF, equals: .expirationDate)
+                    
+                    Spacer(minLength: 0)
+                    
+                    TextField("CVV", text: $cvvCode)
+                        .frame(width: 35)
+                        .focused($activeTF, equals: .cvv)
+                        .keyboardType(.numberPad)
+                    
+                    Image(systemName: "questionmark.circle.fill")
+                }
+                .padding(.top, 15)
+                
+                Spacer(minLength: 0)
+                
+                TextField("CARD HOLDER NAME", text: $cardHolderName)
+                    .focused($activeTF, equals: .cardHolderName)
+                
+            }
+            .padding(20)
+            .environment(\.colorScheme, .dark)
+            .tint(.white)
+        }
+        .frame(height: 200)
+        .padding(.top, 35)
     }
 }
 
